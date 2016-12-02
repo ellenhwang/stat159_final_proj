26ec9ba8946080e44867685efe5cc9c511eeddc2
Joseph Simonian <j.simonian10@gmail.com>
2016-12-01 16:34:54 -0800

    Modify data cleaning script for NAs in regressions

--- code/lasso.R
+++ code/lasso.R
@@ -1,153 +1,80 @@
-## lasso regression for rpy 3 yr
-
 library(glmnet)
 source("functions/functions.R")
 
-#read csv
-rpy3yr <- read.csv("../data/cleaned_data/rpy3yr_tbl.csv",row.names = 1)
-rpy3yr_test <- read.csv("../data/cleaned_data/rpy3yr_test.csv",row.names = 1)
-rpy3yr_train <- read.csv("../data/cleaned_data/rpy3yr_train.csv",row.names = 1)
-#convert them to matrix
-rpy3yr <- as.matrix(rpy3yr)
-rpy3yr_test <- as.matrix(rpy3yr_test)
-rpy3yr_train <- as.matrix(rpy3yr_train)
-
-# Predictors and Response variables
-rpy3yr_x <- rpy3yr[,c(2:11)]
-rpy3yr_y <- rpy3yr[,1]
-
-rpy3yr_test_x <- rpy3yr_test[,c(2:11)]
-rpy3yr_test_y <- rpy3yr_test[,1]
-
-rpy3yr_train_x <- rpy3yr_train[,c(2:11)]
-rpy3yr_train_y <- rpy3yr_train[,1]
-
-col_avgs = apply(rpy3yr_x, 2, mean, na.rm=TRUE)
-
-rows_to_keep_train <- array(apply(rpy3yr_train_x, 1, keep_row)) & complete.cases(rpy3yr_train_y)
-rows_to_keep_test <- array(complete.cases(rpy3yr_test_y))
-
-rpy3yr_train_x <- rpy3yr_train[rows_to_keep_train,c(2:8)]
-rpy3yr_train_y <- rpy3yr_train[rows_to_keep_train,1]
-
-rpy3yr_test_x <- rpy3yr_test[rows_to_keep_test,c(2:8)]
-rpy3yr_test_y <- rpy3yr_test[rows_to_keep_test,1]
-
-rpy3yr_cc <- complete.cases(rpy3yr_x) & complete.cases(rpy3yr_y)
-rpy3yr_x <- rpy3yr[rpy3yr_cc,c(2:8)]
-rpy3yr_y <- rpy3yr[rpy3yr_cc,1]
-
-# Interpolate missing values
-rpy3yr_train_x <- t(apply(rpy3yr_train_x, 1, replace_nas))
-rpy3yr_test_x <- t(apply(rpy3yr_test_x, 1, replace_nas))
-
-
-# The output from the fitting function will give you a
-#list of models (from which you will select the "best" model);
-#save() this output in a .RData file.
-#save(cv.out, file = "")
-grid = 10^seq(10,-2,length =100)
-
+grid = 10^seq(10, -2, length =100)
 set.seed(1)
-rpy3yr_rr_cv_out = cv.glmnet(rpy3yr_train_x,rpy3yr_train_y, alpha = 1, lambda = grid, standardize = FALSE, intercept = FALSE)
+
+## lasso regression for rpy 3 yr
+
+#read csv
+rpy3yr_train_x <- as.matrix(read.csv("../data/cleaned_data/rpy3yr_train_x.csv",row.names = 1))
+rpy3yr_train_y <- as.matrix(read.csv("../data/cleaned_data/rpy3yr_train_y.csv",row.names = 1))
+rpy3yr_test_x <- as.matrix(read.csv("../data/cleaned_data/rpy3yr_test_x.csv",row.names = 1))
+rpy3yr_test_y <- as.matrix(read.csv("../data/cleaned_data/rpy3yr_test_y.csv",row.names = 1))
+
+
+rpy3yr_lr_cv_out = cv.glmnet(rpy3yr_train_x,rpy3yr_train_y, alpha = 1, lambda = grid, standardize = FALSE, intercept = FALSE)
 
 
 # in RR look for $lambda.min from the output of cv.glmnet()
-rpy3yr_rr_bestlam=rpy3yr_rr_cv_out$lambda.min
+rpy3yr_lr_bestlam=rpy3yr_lr_cv_out$lambda.min
 
 
 ##best fitted lambda on test set to calculate MSE
 rpy3yr_lasso <- glmnet(rpy3yr_train_x,rpy3yr_train_y,alpha=1, lambda =grid , intercept = FALSE)
-rpy3yr_lasso_pred <- predict(rpy3yr_lasso, s= rpy3yr_rr_bestlam, newx = rpy3yr_test_x)
+rpy3yr_lasso_pred <- predict(rpy3yr_lasso, s= rpy3yr_lr_bestlam, newx = rpy3yr_test_x)
 rpy3yr_lasso_test_mse <- mean((rpy3yr_lasso_pred - rpy3yr_test_y)^2)
 
 #full data
 rpy3yr_lasso_out <- glmnet(rpy3yr_x,rpy3yr_y, alpha = 1, intercept = FALSE)
-rpy3yr_lasso_full_fit <- predict(rpy3yr_lasso_out, type = "coefficients", s = rpy3yr_rr_bestlam)
-rpy3yr_rr_coef <- as.matrix(rpy3yr_lasso_full_fit)
+rpy3yr_lasso_full_fit <- predict(rpy3yr_lasso_out, type = "coefficients", s = rpy3yr_lr_bestlam)
+rpy3yr_lr_coef <- as.matrix(rpy3yr_lasso_full_fit)
 
 
 # Plot the cross-validation errors in terms of the tunning parameter to visualize which parameter gives the "best" model:
 
 png(file = "../images/rpy3yr_lasso_regression.png")
-plot(rpy3yr_rr_cv_out)
+plot(rpy3yr_lr_cv_out)
 dev.off()
 
 
 
 ## lasso regression for CDR3
 
-
 #read csv
-cdr3 <- read.csv("../data/cleaned_data/cdr3_tbl.csv",row.names = 1)
-cdr3_test <- read.csv("../data/cleaned_data/cdr3_test.csv",row.names = 1)
-cdr3_train <- read.csv("../data/cleaned_data/cdr3_train.csv",row.names = 1)
-#convert them to matrix
-cdr3 <- as.matrix(cdr3)
-cdr3_test <- as.matrix(cdr3_test)
-cdr3_train <- as.matrix(cdr3_train)
+cdr3_train_x <- as.matrix(read.csv("../data/cleaned_data/cdr3_train_x.csv",row.names = 1))
+cdr3_train_y <- as.matrix(read.csv("../data/cleaned_data/cdr3_train_y.csv",row.names = 1))
+cdr3_test_x <- as.matrix(read.csv("../data/cleaned_data/cdr3_test_x.csv",row.names = 1))
+cdr3_test_y <- as.matrix(read.csv("../data/cleaned_data/cdr3_test_y.csv",row.names = 1))
 
-grid = 10^seq(10,-2,length =100)
-
-# Predictors and Response variables
-cdr3_x <- cdr3[,c(2:8)]
-cdr3_y <- cdr3[,1]
-
-cdr3_test_x <- cdr3_test[,c(2:8)]
-cdr3_test_y <- cdr3_test[,1]
-
-cdr3_train_x <- cdr3_train[,c(2:8)]
-cdr3_train_y <- cdr3_train[,1]
-
-# Calculate missing values
-col_avgs = apply(cdr3_x, 2, mean, na.rm=TRUE)
-
-rows_to_keep_train <- array(apply(cdr3_train_x, 1, keep_row)) & complete.cases(cdr3_train_y)
-rows_to_keep_test <- array(complete.cases(cdr3_test_y))
-
-cdr3_train_x <- cdr3_train[rows_to_keep_train,c(2:8)]
-cdr3_train_y <- cdr3_train[rows_to_keep_train,1]
-
-cdr3_test_x <- cdr3_test[rows_to_keep_test,c(2:8)]
-cdr3_test_y <- cdr3_test[rows_to_keep_test,1]
-
-cdr3_cc <- complete.cases(cdr3_x) & complete.cases(cdr3_y)
-cdr3_x <- cdr3[cdr3_cc,c(2:8)]
-cdr3_y <- cdr3[cdr3_cc,1]
-
-# Interpolate missing values
-cdr3_train_x <- t(apply(cdr3_train_x, 1, replace_nas))
-cdr3_test_x <- t(apply(cdr3_test_x, 1, replace_nas))
-
-set.seed(1)
-cdr3_rr_cv_out = cv.glmnet(cdr3_train_x,cdr3_train_y, alpha = 1, lambda = grid, standardize = FALSE, intercept = FALSE)
+cdr3_lr_cv_out = cv.glmnet(cdr3_train_x,cdr3_train_y, alpha = 1, lambda = grid, standardize = FALSE, intercept = FALSE)
 
 
 # in RR look for $lambda.min from the output of cv.glmnet()
-cdr3_rr_bestlam=cdr3_rr_cv_out$lambda.min
+cdr3_lr_bestlam=cdr3_lr_cv_out$lambda.min
 
 
 ##best fitted lambda on test set to calculate MSE
 cdr3_lasso <- glmnet(cdr3_train_x,cdr3_train_y,alpha=1, lambda =grid , intercept = FALSE)
-cdr3_lasso_pred <- predict(cdr3_lasso, s= cdr3_rr_bestlam, newx = cdr3_test_x)
+cdr3_lasso_pred <- predict(cdr3_lasso, s= cdr3_lr_bestlam, newx = cdr3_test_x)
 cdr3_lasso_test_mse <- mean((cdr3_lasso_pred - cdr3_test_y)^2)
 
 #full data
 cdr3_lasso_out <- glmnet(cdr3_x,cdr3_y, alpha = 1, intercept = FALSE)
-cdr3_lasso_full_fit <- predict(cdr3_lasso_out, type = "coefficients", s = cdr3_rr_bestlam)
-cdr3_rr_coef <- as.matrix(cdr3_lasso_full_fit)
+cdr3_lasso_full_fit <- predict(cdr3_lasso_out, type = "coefficients", s = cdr3_lr_bestlam)
+cdr3_lr_coef <- as.matrix(cdr3_lasso_full_fit)
 
 
 # Plot the cross-validation errors in terms of the tunning parameter to visualize which parameter gives the "best" model:
 
 png(file = "../images/cdr3_lasso_regression.png")
-plot(cdr3_rr_cv_out)
+plot(cdr3_lr_cv_out)
 dev.off()
 
 
-save(rpy3yr_rr_cv_out, cdr3_rr_cv_out,
-     rpy3yr_rr_bestlam, cdr3_rr_bestlam,
+save(rpy3yr_lr_cv_out, cdr3_lr_cv_out,
+     rpy3yr_lr_bestlam, cdr3_lr_bestlam,
      rpy3yr_lasso_test_mse, cdr3_lasso_test_mse,
      rpy3yr_lasso_full_fit, cdr3_lasso_full_fit,
-     rpy3yr_rr_coef, cdr3_rr_coef,
+     rpy3yr_lr_coef, cdr3_lr_coef,
      file = "../data/lasso.RData" )
