5019f553876cee50b9e43facfe8b314795e0bcff
Ellen Hwang <ellenhwng@gmail.com>
2016-12-03 13:23:19 -0800

    ols make fixed errors

--- code/regression_scripts/ols_script.R
+++ code/regression_scripts/ols_script.R
@@ -1,8 +1,8 @@
 # ################################################################
 # OLS Regression
 # ################################################################
-# load tables
-load("../../data/cleaned_predictor_tables.RData")
+rpy_3yr_tbl <- read.csv('../../data/cleaned_data/rpy3yr_tbl.csv')
+cdr3_tbl <- read.csv('../../data/cleaned_data/cdr3_tbl.csv')
 
 rpy_3yr_reg <- lm(RPY_3YR_RT ~ ., data = rpy_3yr_tbl)
 rpy_3yr_regsum <- summary(rpy_3yr_reg)
@@ -10,4 +10,4 @@ rpy_3yr_regsum <- summary(rpy_3yr_reg)
 cdr3_reg <- lm(CDR3 ~ ., data = cdr3_tbl)
 cdr3_regsum <- summary(cdr3_reg)
 
-save(rpy_3yr_reg, rpy_3yr_regsum, cdr3_reg, cdr3_regsum, file = '../../data/RData/ols-regression.RData')
+save(rpy_3yr_reg, rpy_3yr_regsum, cdr3_reg, cdr3_regsum, file = '../../data/RData/ols.RData')
